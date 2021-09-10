ca71ec574efd400b9005b556a9009472b927a906
Nathan Rose <fullstackroses@gmail.com>
2021-09-09 20:47:38 -0500

    working button javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,13 @@
-
-//button
 var buttonE1 = document.querySelector("#save-task");
-console.log(buttonE1);
+var tasksToDoE1 = document.querySelector("#tasks-to-do");
+
+
+ var createTaskHandler = function() {
+    var listItemE1 = document.createElement("li");
+    listItemE1.className = "task-item";
+    listItemE1.textContent = "This is a new task bitch fuck.";
+    tasksToDoE1.appendChild(listItemE1);
+};
+
+ buttonE1.addEventListener("click", createTaskHandler); 
+ 
